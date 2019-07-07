52d660b4e77be623bcc189c33a92581ea9ccd549
Vincent Barthelemy <vincent.barthelemy@djit.fr>
2019-07-07 15:31:34 +0200

    [Experimentation][GameLoop] Pass graveyard tests for actor-based game loop implementation

--- src/test/kotlin/fr/tvbarthel/mtg/experimentation/GraveyardTest.kt
+++ src/test/kotlin/fr/tvbarthel/mtg/experimentation/GraveyardTest.kt
@@ -1,43 +1,56 @@
 package fr.tvbarthel.mtg.experimentation
 
+import fr.tvbarthel.mtg.experimentation.actorgameloop.ActorGameLoop
+import io.kotlintest.data.forall
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import io.kotlintest.tables.row
 
 class GraveyardTest : StringSpec({
     "Instant casted are added to graveyard" {
-        // Given
-        val player1 = ScriptedPlayer("Ava")
-        val player2 = ScriptedPlayer("Williams")
-        val fakeInstant = FakeInstant("p1")
+        forall(
+            row(FirstNaiveGameLoop()),
+            row(ActorGameLoop())
+        ) { gameLoop ->
+            // Given
+            val player1 = ScriptedPlayer("Ava")
+            val player2 = ScriptedPlayer("Williams")
+            val fakeInstant = FakeInstant("p1")
 
-        // When
-        ScriptedActionBuilder(player1, player2)
-            // Turn 0 - player 1 active
-            .addTurn(Step.FirstMainPhaseStep, player1, CastInstantAction(fakeInstant))
-            .playTurns(instantiateGameLoop())
+            // When
+            ScriptedActionBuilder(player1, player2)
+                // Turn 0 - player 1 active
+                .addTurn(Step.FirstMainPhaseStep, player1, CastInstantAction(fakeInstant))
+                .playTurns(gameLoop)
 
-        // Then
-        player1.graveyard.size shouldBe 1
-        player1.graveyard[0] shouldBe fakeInstant
+            // Then
+            player1.graveyard.size shouldBe 1
+            player1.graveyard[0] shouldBe fakeInstant
+        }
     }
 
     "Creature killed are added to graveyard" {
-        // Given
-        val player1 = ScriptedPlayer("Ava")
-        val player2 = ScriptedPlayer("Williams")
-        val sanctuaryCat = SanctuaryCat("p2")
-        val shock = Shock("p1", sanctuaryCat)
+        forall(
+            row(FirstNaiveGameLoop()),
+            row(ActorGameLoop())
+        ) { gameLoop ->
+            // Given
+            val player1 = ScriptedPlayer("Ava")
+            val player2 = ScriptedPlayer("Williams")
+            val sanctuaryCat = SanctuaryCat("p2")
+            val shock = Shock("p1", sanctuaryCat)
 
-        player2.board.add(sanctuaryCat)
+            player2.board.add(sanctuaryCat)
 
-        // When
-        ScriptedActionBuilder(player1, player2)
-            // Turn 0 - player 1 active
-            .addTurn(Step.FirstMainPhaseStep, player1, CastInstantAction(shock))
-            .playTurns(instantiateGameLoop())
+            // When
+            ScriptedActionBuilder(player1, player2)
+                // Turn 0 - player 1 active
+                .addTurn(Step.FirstMainPhaseStep, player1, CastInstantAction(shock))
+                .playTurns(instantiateGameLoop())
 
-        // Then
-        player2.graveyard.size shouldBe 1
-        player2.graveyard[0] shouldBe sanctuaryCat
+            // Then
+            player2.graveyard.size shouldBe 1
+            player2.graveyard[0] shouldBe sanctuaryCat
+        }
     }
 })
