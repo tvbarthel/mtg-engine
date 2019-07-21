da3029e2444cccab89dde796b29ac32b8408a466
tbarthel-fr <thomas.barthelemy.utc@gmail.com>
2019-07-21 15:59:43 +0200

    [Card] add concept of property and modifier

--- src/main/kotlin/fr/tvbarthel/mtg/engine/Card.kt
+++ src/main/kotlin/fr/tvbarthel/mtg/engine/Card.kt
@@ -6,7 +6,13 @@ package fr.tvbarthel.mtg.engine
  *
  * https://mtg.gamepedia.com/Card
  */
-class Card {
+open class Card(
+    val id: Int,
+    val types: List<Type>,
+    val manaCost: Property<Int> = Property(0),
+    val power: Property<Int> = Property(0),
+    val toughness: Property<Int> = Property(0)
+) {
 
     /**
      * Card type is a characteristic found on every Magic: the Gathering card. It appears in the type line,
@@ -19,4 +25,40 @@ class Card {
      * https://mtg.gamepedia.com/Card_type
      */
     open class Type
+
+
+    /**
+     * Hold the value of any properties that could be attached to a card.
+     */
+    class Property<T>(
+        private val initialValue: T,
+        private val modifiers: MutableList<Modifier<T>> = mutableListOf()
+    ) {
+        /**
+         * Current value of the property.
+         *
+         * Include the modification applied by every modifier currently set to the property.
+         */
+        fun value(): T = modifiers.fold(initialValue) { v, m -> m.modified(v) }
+
+        /**
+         * Add a given property modifier.
+         */
+        fun addModifier(modifier: Modifier<T>) = modifiers.add(modifier)
+
+        /**
+         * Remove a given property modifier.
+         */
+        fun removeModifier(modifier: Modifier<T>): Boolean = modifiers.remove(modifier)
+    }
+
+    /**
+     * Allow to modify the value of a property.
+     */
+    abstract class Modifier<T> {
+        /**
+         * Must modify the current value of the property and return a new one.
+         */
+        abstract fun modified(value: T): T
+    }
 }
