657bb791173d9dbf44c55f087ba40034d70d3f27
tbarthel-fr <thomas.barthelemy.utc@gmail.com>
2019-07-21 15:07:02 +0200

    [Mulligan] fix shuffling when mulligan is applied

--- src/main/kotlin/fr/tvbarthel/mtg/engine/opening/MulliganHandAction.kt
+++ src/main/kotlin/fr/tvbarthel/mtg/engine/opening/MulliganHandAction.kt
@@ -4,13 +4,17 @@ import fr.tvbarthel.mtg.engine.Action
 import fr.tvbarthel.mtg.engine.Card
 import fr.tvbarthel.mtg.engine.Event
 import fr.tvbarthel.mtg.engine.GameState
+import kotlin.random.Random
 
 /**
  * Action used to mulligan the given hand and request a new staring hand.
  */
 class MulliganHandAction(val hand: List<Card>, private val playerId: Int) : Action() {
     override fun apply(state: GameState): List<Event> {
-        state.players.find { it.id == playerId }!!.mulligan += 1
+        state.players.find { it.id == playerId }!!.let { player ->
+            player.mulligan += 1
+            player.library.shuffle(Random(state.seed))
+        }
         return emptyList()
     }
 }
